e082cf6b73b580b99a7cbb32962fd7c9863b32a5
Body Fa <tcs223647@gmail.com>
2026-08-06 22:14:25 +0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="border-t border-zinc-200 bg-white py-8">
+    <footer className=" bg-none py-8">
       <div className="mx-auto flex max-w-6xl items-center justify-center px-4 text-center text-xs font-mono italic text-zinc-500 md:px-8">
         "Simplicity is prerequisite for reliability." — Edsger W. Dijkstra
       </div>
