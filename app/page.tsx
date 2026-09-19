a3e7737a6b2864a1f9f5f2b74b115eaac53d207d
Body Fa <tcs223647@gmail.com>
2026-09-20 04:40:23 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -122,7 +122,7 @@ export default function Home() {
 
         <section className="portfolio-screen skills-screen">
           <motion.div
-            className="glass-panel skills-panel skills-glass"
+            className="glass-panel skills-panel "
             initial={{ opacity: 0, scale: 0.96, y: 35 }}
             whileInView={{ opacity: 1, scale: 1, y: 0 }}
             viewport={{ once: true, amount: 0.3 }}
